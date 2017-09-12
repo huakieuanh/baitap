19e054822d71e0525acb847ecb5e90c9276b5043
huakieuanh <huakieuanh@gmail.com>
2017-09-12 19:31:59 +0900

    fix

--- javascript/baitap1109.js
+++ javascript/baitap1109.js
@@ -39,3 +39,40 @@ function Clear(){
   p = -1
   $(".show_kq").text(0)
 }
+
+function Ketqua(n){
+  x=[0,1,2,3,4,5,6,7,8,9];
+  a=""
+  b=""
+  var i=0;
+  for(i=0;i<x.length;i++){
+    if(n == x[i] && p == -1){
+      a = a+n
+      a = Number("a")
+    }
+    else if(n == x[i] && p !=-1){
+      b = b+n
+      b = Number("b")
+    }
+    else if(n!=x[i]){
+      p = n
+    }
+  }
+   $(".show_kq").text(a,b,p)
+  var kq=0
+  if(p=="+"){
+    kq=kq+(a+b)
+  }
+  else if(p=="-"){
+    kq=kq+(a-b)
+  }
+  else if(p=="x"){
+    kq=kq+(a*b)
+  }
+  else if(p==":"){
+    kq=kq+(a/b)
+  }
+  a = -1
+  b = -1
+  p = -1
+  $(".show_kq").text(kq)}
